7ab5e3de1e341823bdb964b254be5d00a841439e
munzirulalom <alomb008@gmail.com>
2021-12-07 13:50:52 +0600

    Initial

---
+++ function.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              #
+ * @since             1.0.0
+ * @package           Sgca_Api
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SGCA API
+ * Plugin URI:        #
+ * Description:       Display recent post from another WP website using WP REST API.
+ * Version:           1.0.0
+ * Author:            SGCArena
+ * Author URI:        https://www.sgcarena.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       sgca-api
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'SGCA_API_VERSION', '1.0.0' );
